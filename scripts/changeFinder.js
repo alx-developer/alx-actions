98807bac22ea80423e7462a901d480310204f149
Alejandro Lopez <program.alopez@gmail.com>
2023-07-17 21:23:23 -0500

    chore: split script for workflow

---
+++ scripts/changeFinder.js
@@ -0,0 +1,33 @@
+module.exports = async ({ github, context, core }) => {
+  const { execSync } = require('child_process');
+  const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
+  try {
+    const latestRelease = await github.rest.repos.getLatestRelease({
+      owner,
+      repo,
+    });
+    console.log(`latest release: ${JSON.stringify(latestRelease.data)}`);
+  } catch (e) {
+    console.log('fails get latest release');
+  }
+  try {
+    execSync('git pull --tags');
+    execSync(`git reset --hard ${latestRelease.data.tag_name}`);
+  } catch (e) {
+    console.log('fails remove last release tag');
+  }
+  const status = execSync(`git diff --name-only origin/staging`, {
+    encoding: 'utf-8',
+  });
+  console.log(status);
+  const changes = status.split('\n');
+  let nodePaths = new Set();
+  for (const change of changes) {
+    if (change.startsWith('libs/')) {
+      const library = change.split('/')[1];
+      nodePaths.add(library);
+    }
+  }
+  nodePaths = Array.from(nodePaths);
+  return nodePaths;
+};
